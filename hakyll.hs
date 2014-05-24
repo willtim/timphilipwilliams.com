e69f3ba83895d22d378dd856190fc11e001c889c
Tim Williams <tim.philip.williams@gmail.com>
2014-05-24 21:09:39 +0100

    ported to Hakyll 4

--- hakyll.hs
+++ hakyll.hs
@@ -1,194 +1,185 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude hiding (id)
-import Control.Category (id)
-import Control.Arrow ((>>>), (***), arr)
-import Data.Monoid (mappend, mempty, mconcat)
+import Control.Applicative
+import Control.Monad
 import Data.Char (toLower)
+import Data.List
+import Data.Maybe
+import Data.Monoid
+import System.FilePath
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 import Text.Pandoc
 import Hakyll
 
-config = defaultHakyllConfiguration
+config :: Configuration
+config = defaultConfiguration
 
 feedConfiguration = FeedConfiguration
     { feedTitle       = "timphilipwilliams.com"
     , feedDescription = "A functional programming blog."
     , feedAuthorName  = "Tim Williams"
     , feedAuthorEmail = "info@timphilipwilliams.com"
-    , feedRoot        = "http://timphilipwilliams.com"
+    , feedRoot        = "http://www.timphilipwilliams.com"
     }
 
 main :: IO ()
 main = hakyllWith config $ do
-              
+
     -- Read templates
     match "templates/*" $ compile templateCompiler
+
+    -- Build tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
 
     -- Render each post
     match "posts/*" $ do
       route $ setExtension ".html"
-      compile $ myPageCompiler
-            >>> applyTemplateCompilers ["post", "default", "scaffold"]
-            >>> relativizeUrlsCompiler
+      compile $ myPandocCompiler
+            >>= applyTemplates ["post", "default", "scaffold"] myDefaultCtx
+            >>= relativizeUrls
 
     -- site index - currently a list of all my posts
-    match "index.html" $ route idRoute
-    create "index.html" $ constA mempty
-      >>> addMyDefaultFields
-      >>> arr (setField "title" "Posts")
-      >>> arr (setField "postsurl" nullLink)
-      >>> arr (setField "postsclass" "active")
-      >>> setFieldPageList recentFirst "templates/postitem.html" "posts" "posts/*"
-      >>> applyTemplateCompilers ["posts", "default", "scaffold"]
-      >>> relativizeUrlsCompiler    
-    
+    create ["index.html"] $ do
+      route idRoute
+      compile $ do
+        items   <- recentFirst =<< loadAll "posts/*"
+        itemTpl <- loadBody "templates/postitem.html"
+        list    <- applyTemplateList itemTpl myDefaultCtx items
+        let ctx =  constField "title" "Posts"
+                <> constField "postsurl" nullLink
+                <> constField "postsclass" "active"
+                <> constField "posts" list
+                <> myDefaultCtx
+        makeItem ""
+          >>= applyTemplates ["posts", "default", "scaffold"] ctx
+          >>= relativizeUrls
+
     match "slides.html" $ do
-        route   $ idRoute
-        compile $ readPageCompiler
-            >>> addMyDefaultFields
-            >>> arr (setField "title" "Slides")
-            >>> arr (setField "slidesurl" nullLink)
-            >>> arr (setField "slidesclass" "active")
-            >>> applyTemplateCompilers ["default", "scaffold"]
-            >>> relativizeUrlsCompiler
+        route   idRoute
+        let ctx =  constField "title" "Slides"
+                <> constField "slidesurl" nullLink
+                <> constField "slidesclass" "active"
+                <> myDefaultCtx
+        compile $ getResourceBody
+              >>= applyTemplates ["default", "scaffold"] ctx
+              >>= relativizeUrls
 
     match "aboutme.html" $ do
-        route   $ idRoute
-        compile $ readPageCompiler
-            >>> addMyDefaultFields
-            >>> arr (setField "title" "About me")
-            >>> arr (setField "aboutmeurl" nullLink)
-            >>> arr (setField "aboutmeclass" "active")
-            >>> applyTemplateCompilers ["default", "scaffold"]
-            >>> relativizeUrlsCompiler
+        route   idRoute
+        let ctx =  constField "title" "About me"
+                <> constField "aboutmeurl" nullLink
+                <> constField "aboutmeclass" "active"
+                <> myDefaultCtx
+        compile $ getResourceBody
+              >>= applyTemplates ["default", "scaffold"] ctx
+              >>= relativizeUrls
 
-    match "tags.html" $ do
-        route   $ idRoute
-        compile $ readPageCompiler
-            >>> addMyDefaultFields
-            >>> arr (setField "title" "Tags")
-            >>> arr (setField "tagsurl" nullLink)
-            >>> arr (setField "tagsclass" "active")
-            >>> applyTemplateCompilers ["default", "scaffold"]
-            >>> relativizeUrlsCompiler
+    tagsRules tags $ \tag pattern -> do
+        route tagRoute
+        compile $ do
+            items   <- recentFirst =<< loadAll pattern
+            itemTpl <- loadBody "templates/postitem.html"
+            list    <- applyTemplateList itemTpl myDefaultCtx items
+            let ctx =  constField "tag"   tag
+                    <> constField "posts" list
+                    <> myDefaultCtx
+            makeItem ""
+              >>= applyTemplates ["postswithtag", "default", "scaffold"] ctx
+              >>= relativizeUrls
 
-    -- Copy fonts
+    create ["tags.html"] $ do
+        route idRoute
+        compile $ do
+            cloud <- renderTagCloud 150 300 tags
+            let ctx =  constField "title" "Tags"
+                    <> constField "tagsurl" nullLink
+                    <> constField "tagsclass" "active"
+                    <> constField "tags" cloud
+                    <> myDefaultCtx
+            makeItem ""
+              >>= applyTemplates ["tags", "default", "scaffold"] ctx
+              >>= relativizeUrls
+
     match "font/*" $ do
-        route   $ idRoute
-        compile $ copyFileCompiler
+        route   idRoute
+        compile copyFileCompiler
 
-    -- Copy CSS
     match "css/*.css" $ do
-        route   $ idRoute
-        compile $ compressCssCompiler
+        route   idRoute
+        compile compressCssCompiler
 
-    -- Copy Javascript
     match "js/*.js" $ do
-        route   $ idRoute
-        compile $ copyFileCompiler
+        route   idRoute
+        compile copyFileCompiler
 
-    -- Copy images
     match "img/*" $ do
-        route   $ idRoute
-        compile $ copyFileCompiler
+        route   idRoute
+        compile copyFileCompiler
 
-    -- Copy slides
     match "slides/*" $ do
-        route   $ idRoute
-        compile $ copyFileCompiler
+        route   idRoute
+        compile copyFileCompiler
 
-    -- Tags
-    create "tags" $
-        requireAll "posts/*" (\_ ps -> readTags ps :: Tags String)
-
-    -- Add a tag list compiler for every tag
-    match "tag/*" $ route $ tagRoute
-    metaCompile $ require_ "tags"
-        >>> arr tagsMap
-        >>> arr (map (\(t, p) -> (tagIdentifier "tag/*" t, 
-                                  makePostList "Posts with the tag" t p)))
-
-    -- Render RSS feed
-    match "rss.xml" $ route idRoute
-    create "rss.xml" $ requireAll_ "posts/*" >>> renderRss feedConfiguration
+    create ["rss.xml"] $ do
+      route idRoute
+      compile $ loadAll "posts/*"
+        >>= renderRss feedConfiguration myDefaultCtx
 
     -- 404
     -- Don't relativize URLs
     match "static/404.html" $ do
         route   $ constRoute "404.html"
-        compile $ readPageCompiler
-            >>> addMyDefaultFields
-            >>> arr (setField "postsclass" "")
-            >>> arr (setField "title" "404")
-            >>> arr (setField "robots" "noindex, nofollow, noarchive, nocache")
-            >>> applyTemplateCompilers ["default", "scaffold"]
+        let ctx =  constField "postsclass" ""
+                <> constField "title" "404"
+                <> constField "robots" "noindex, nofollow, noarchive, nocache"
+                <> myDefaultCtx
+        compile $ getResourceBody
+            >>= applyTemplates ["default", "scaffold"] ctx
 
 -- | Consistent convention for links that don't go anywhere
 nullLink :: String
 nullLink = "javascript:void(0)"
 
--- | Default setup is for individual post pages
-addMyDefaultFields = arr (trySetField "robots" "index, follow")
-    >>> arr (trySetField "postsclass" "")
-    >>> arr (trySetField "slidesclass" "")
-    >>> arr (trySetField "aboutmeclass" "")
-    >>> arr (trySetField "tagsclass" "")
-    >>> arr (trySetField "postsurl" "/")
-    >>> arr (trySetField "slidesurl" "/slides.html")
-    >>> arr (trySetField "aboutmeurl" "/aboutme.html")
-    >>> arr (trySetField "tagsurl" "/tags.html")
-    >>> arr (trySetField "author" "Tim Williams")
-    >>> arr (renderDateField "date" "%e %B %Y" "")
-
 -- | Allow for reference style links in markdown
 --   and disable the leading >
-pandocWriteOptions = defaultWriterOptions {
-      writerReferenceLinks = True
-    , writerLiterateHaskell = False
+pandocWriteOptions :: WriterOptions
+pandocWriteOptions = defaultHakyllWriterOptions
+    {  writerExtensions     = S.delete Ext_literate_haskell
+                                (writerExtensions defaultHakyllWriterOptions)
+     , writerReferenceLinks = True
     }
 
-myPageCompiler :: Compiler Resource (Page String)
-myPageCompiler = readPageCompiler
-    >>> addDefaultFields
-    >>> addMyDefaultFields
-    >>> arr applySelf
-    >>> pageRenderPandocWith defaultHakyllParserState pandocWriteOptions
 
-applyTemplateCompilers :: [String] -> Compiler (Page String) (Page String)
-applyTemplateCompilers [] = arr id
-applyTemplateCompilers (x:xs) = applyTemplateCompiler templ >>> applyTemplateCompilers xs
-    where templ = parseIdentifier ("templates/" ++ x ++ ".html")
+myPandocCompiler = pandocCompilerWith defaultHakyllReaderOptions pandocWriteOptions
 
-tagIdentifier :: Pattern (Page String) -> String -> Identifier (Page String)
-tagIdentifier pattern = fromCapture pattern
+-- | Default setup is for individual post pages
+myDefaultCtx :: Context String
+myDefaultCtx = mconcat [
+        constField "robots" "index, follow"
+      , constField "homeclass" "active"
+      , constField "homeurl" "/index.html"
+      , constField "postsclass"  ""
+      , constField "slidesclass" ""
+      , constField "aboutmeclass"  ""
+      , constField "tagsclass"  ""
+      , constField "postsurl"  "/"
+      , constField "slidesurl"  "/slides.html"
+      , constField "aboutmeurl"  "/aboutme.html"
+      , constField "tagsurl"  "/tags.html"
+      , constField "author" "Tim Williams"
+      , dateField "date" "%e %B %Y"
+      , defaultContext
+      ]
 
--- | Auxiliary compiler: generate a post list from a list of given posts, and
---   add it to the current page under @$posts@
-addPostList :: String -> Identifier (Template) -> Compiler (Page String, [Page String]) (Page String)
-addPostList field template = setFieldA field $
-    arr recentFirst
-        >>> require template (\p t -> map (applyTemplate t) p)
-        >>> arr mconcat
-        >>> arr pageBody
-        
-makePostList :: String
-            -> String
-            -> [Page String]
-            -> Compiler () (Page String)
-makePostList message tag posts =
-    constA (mempty, posts)
-        >>> addPostList "posts" "templates/postitem.html"
-        >>> addMyDefaultFields
-        >>> arr (setField "title" (message ++ " &#8216;" ++ tag ++ "&#8217;"))
-        >>> arr (setField "tag" tag)
-      --  >>> arr (setField "route" (adjustLink tag))
-        >>> applyTemplateCompilers ["postswithtag", "default", "scaffold"]
-        >>> relativizeUrlsCompiler
+applyTemplates :: [String] -> Context String -> Item String -> Compiler (Item String)
+applyTemplates names ctx = foldr apply return names
+    where
+      apply name = (loadAndApplyTemplate (templ name) ctx >=>)
+      templ name = fromFilePath $ "templates/" ++ name ++ ".html"
 
--- TODO clean up
 tagRoute :: Routes
 tagRoute = foldl1 composeRoutes
     [ setExtension ".html"
